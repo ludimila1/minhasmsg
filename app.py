3c08b595568d7db579cce445f9b18172ebf6c030
Carmosoeles <carmosoeles16@gmail.com>
2019-11-07 14:58:16 -0300

    addei templates index e mensagens

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "pagina inicial"
+    return render_template("index.html")
+
+@app.route("/nova-mensagem")
+def novaMensagem():
+    return render_template("mensagens.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
