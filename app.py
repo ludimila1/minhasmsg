77f9917ea6b32ff0de5adef825c02c855d46bb78
Carmosoeles <carmosoeles16@gmail.com>
2019-11-07 17:27:43 -0300

    atualizei tudo

--- app.py
+++ app.py
@@ -1,28 +1,40 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlquemy
+from flask_sqlalchemy import SQLAlchemy
+from formulario import MensagemForm
 
 app = Flask(__name__)
-db=SQLAlquemy(app)
+db=SQLAlchemy(app)
 
-app.config["SCLALCHEMY_DATABASE_URI"] = "sqlite:///minhasmensagens.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minhasmensagens.db'
+app.config['SECRET_KEY'] = 'lsrhvanlvlqtaçcojgq'
 
 class Mensagens(db.Model):
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     mensagem = db.Column(db.Text, nullable=False)
 
     def __repre__(self):
-        return "<<<MSG: %r>>>"self.mensagem
+        return self.mensagem
 
-
-
+   
 @app.route("/")
 def index():
-    return render_template("index.html")
+    msgs=Mensagens.query.all()
+    return render_template("index.html", msgs=msgs )
 
-@app.route("/nova-mensagem")
+
+@app.route("/nova-mensagem", methods=["POST", "GET"])
 def novaMensagem():
-    return render_template("mensagens.html")
+    form = MensagemForm()
 
+    if form.validate_on_submit():
+
+        msg= Mensagens()
+        msg.mensagem=form.mensagem.data
+        db.session.add(msg)
+        db.session.commit()
+        
+    return render_template("mensagens.html", form=form)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
